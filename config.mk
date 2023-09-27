00a3614858738dd9ea3402a25e4e2bfe41321c83
alpheratz0 <alpheratz99@protonmail.com>
2023-09-27 20:44:17 -0300

    clean up config.mk

--- config.mk
+++ config.mk
@@ -2,14 +2,19 @@
 # This program is free software.
 
 VERSION = 0.9.2
+
 PREFIX = /usr/local
 MANPREFIX = $(PREFIX)/share/man
 APPPREFIX = $(PREFIX)/share/applications
+
 PKG_CONFIG = pkg-config
-DEPENDENCIES = fcft xcb xcb-shm xcb-image xcb-keysyms xcb-xkb \
-			   libpulse pixman-1
+
+DEPENDENCIES = fcft xcb xcb-shm xcb-image xcb-keysyms xcb-xkb libpulse pixman-1
+
 INCS = $(shell $(PKG_CONFIG) --cflags $(DEPENDENCIES)) -Iinclude
 LIBS = $(shell $(PKG_CONFIG) --libs $(DEPENDENCIES))
+
 CFLAGS = -Os $(INCS) $(CPPFLAGS) -DVERSION=\"$(VERSION)\"
 LDFLAGS = -s $(LIBS)
+
 CC = cc
